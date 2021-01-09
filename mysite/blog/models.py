4a3743da2a6bae9f5702b00e7156e4546d8463dd
C9-LinkRs <linkrsfan@gmail.com>
2021-01-09 21:04:56 +0100

    Models done

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,84 @@
 from django.db import models
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
+
+POST_STATUS = (
+  ("draft", "Draft"),
+  ("published", "Published")
+)
+
+TRANSACTIONS_TYPES = (
+  (0, "Post_creation"),
+  (1, "Post_deletion"),
+  (2, "Post_modification"),
+  (3, "Post_comment"),
+  (4, "Post_like"),
+  (5, "User_creation")
+)
 
 # Create your models here.
+class Category(models.Model):
+  name = models.CharField(max_length=200, unique=True)
+  slug = models.SlugField(max_length=200, unique=True)
+
+  class Meta:
+    ordering = ("name",)
+    verbose_name = "category"
+    verbose_plural_name = "categories"
+  
+  def __str__(self):
+    return f"<Category: {self.name}>"
+
+
+class Post(models.Model):
+  title = models.CharField(max_length=300, unique=True)
+  slug = models.SlugField(max_length=200, unique=True)
+  author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+  content = models.TextField()
+  likes = models.ManyToManyField(User, related_name="likes")
+  tags = TaggableManager()
+  category = models.ForeignKey(Category)
+  status = models.CharField(choices=POST_STATUS, default="draft")
+  publish_date = models.DateTimeField()
+  deactivate_date = models.DateTimeField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  class Meta:
+    ordering = ["-created-at"]
+  
+  def __str__(self):
+    return f"<Post: {self.title}>"
+  
+  @property
+  def number_of_comments(self):
+    return PostComent.objects.filter(post=self).count()
+  
+  @property
+  def number_of_likes(self):
+    return self.likes.count()
+
+
+class PostComment(models.Model):
+  post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="blog_comments")
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  content = models.TextField()
+  posted_at = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    ordering = ["-posted_at"]
+  
+  class __str__(self):
+    return f"<PostComent: {self.author.username}, {self.post.title}>"
+
+
+class Transaction(models.Model):
+  author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_transactions")
+  transaction_type = models.IntegerField(choices=TRANSACTIONS_TYPES)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    ordering = ["-created-at"]
+
+  def __str__(self):
+    return f"<Transaction: {self.author.username}, {self.transaction_type}>"
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'taggit'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get("SQL_ENGINE"),
+        'NAME': os.environ.get("POSTGRES_DB"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("POSTGRES_HOST"),
+        'PORT': os.environ.get("POSTGRES_PORT"),
     }
 }
 
