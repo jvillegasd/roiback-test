6024eb4334a818b79d22bec734b9fef07b54f08e
C9-LinkRs <linkrsfan@gmail.com>
2021-01-15 07:08:29 +0100

    managers commented

--- mysite/blog/managers.py
+++ mysite/blog/managers.py
@@ -4,8 +4,19 @@ from django.db.models import Q
 
 # Customs Queries Sets
 class PostQuerySet(models.query.QuerySet):
+  """
+    Post query set.
+
+    Custom model QuerySet for Post model. This queryset contains
+    some queries usefull for post filtering and which post can be
+    considered as 'Published'. So, they can be shown on users feed.
+  """
 
   def get_published_posts(self):
+    """
+      This function return published posts. It takes into accounts
+      posts 'publish date', their 'post status' and their 'deactivate_date'.
+    """
     now_date = datetime.datetime.now()
     posts = self.filter(
       Q(publish_date__lte=now_date) &
@@ -16,17 +27,32 @@ class PostQuerySet(models.query.QuerySet):
     return posts
   
   def get_posts_by_category(self, category):
+    """
+      This functions returns every posts filtered by a category.
+    """
     return self.filter(category=category)
   
   def get_posts_by_author(self, author):
+    """
+      This functions returns every posts filtered by an author.
+    """
     return self.filter(author=author)
   
   def get_posts_by_tags(self, tags):
+    """
+      This functions returns every posts filtered by a tag.
+    """
     return self.filter(tags__name__in=tags)
 
 
 # Custom Managers
 class PostManager(models.Manager):
+  """
+    Post manager.
+
+    This model manager is used to modify default Post model manager for
+    use defined custom QuerySet class methods.
+  """
 
   def get_queryset(self):
     return PostQuerySet(self.model, using=self._db)
